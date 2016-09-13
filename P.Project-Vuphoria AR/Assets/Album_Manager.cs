b321a4c463725f9c98345bb83458ebac7d7c6262
Martin Relevo <martin.relevo@hotmail.co.uk>
2016-09-13 18:33:39 +0100

    AR Project 1/PhotoAlbum Beta Done
    Beta:
    -Spawn Picture frame and picture
    -UI buttons to shift through pictures
    -UI to restart and close APP
    
    Problem:
    -Object is white when initially spawned. Quality becomes better when
    getting closer to the object after spawn.
    
    Plans:
    -Fix 'White' object spawn

---
+++ P.Project-Vuphoria AR/Assets/Album_Manager.cs
@@ -0,0 +1,74 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+
+public class Album_Manager : MonoBehaviour
+{
+    private Renderer rend;
+    private Material[] mats;
+    private int CurrentStrip;
+    private int MaxStrip;
+
+    public List<Material> List_Strips;
+
+    void Awake()
+    {
+        CurrentStrip = 0;
+        MaxStrip = 2;
+        rend = GetComponent<Renderer>();
+        mats = rend.materials;
+        mats[1] = List_Strips[CurrentStrip];
+        rend.materials = mats;
+    }
+
+	// Use this for initialization
+	void Start ()
+    {
+
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        mats[1] = List_Strips[CurrentStrip];
+        rend.materials = mats;
+    }
+
+    public void ForwardStrip()
+    {
+        if (CurrentStrip > MaxStrip)
+        {
+            CurrentStrip = 0;
+        }
+
+        if (CurrentStrip < MaxStrip)
+        {
+            CurrentStrip = CurrentStrip + 1;
+        }
+     }
+
+    public void BackwardStrip()
+    {
+        if (CurrentStrip < 0)
+        {
+            CurrentStrip = MaxStrip;
+        }
+
+        if (CurrentStrip > 0)
+        {
+            CurrentStrip = CurrentStrip - 1;
+        }
+    }
+
+    public void StartStrip()
+    {
+        CurrentStrip = 0;
+    }
+
+    public void LastStrip()
+    {
+        CurrentStrip = MaxStrip;
+    }
+}
---
+++ P.Project-Vuphoria AR/Assets/Independent_UI.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class Independent_UI : MonoBehaviour {
+
+	// Use this for initialization
+	void Start ()
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	
+	}
+    public void Appclose()
+    {
+        Application.Quit();
+    }
+
+    public void AppRestartScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+}
